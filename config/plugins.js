363c8d2350776f912baa74c96fb47a7eb0d11b48
felipesaev <felipesaev@gmail.com>
2022-01-18 18:29:20 -0300

    create api for content

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = {
+  //
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
+
+ 
+
+ 
